5069e40da5b1e7373428b1cbce1a7c9f139f9da7
smstalker <sarah.stalker15@outlook.com>
2022-02-27 20:50:18 -0700

    start of menu

--- week4.js
+++ week4.js
@@ -10,3 +10,31 @@ class Vehicle {
         return`${this.model} was made in ${this.year}.`
     }
 }
+
+class Department {
+    constructor(name) {
+        this.name = name;
+        this.vehicles = [];
+    }
+    addVehicle(vehicle) {
+    if (vehicle instanceof Vehicle) {
+       this.vehicles.push(vehicle);     
+    } else {
+        throw new Error ('not an instance of vehicle')
+    }
+} 
+describe(){
+return `${this.name} has ${this.vehicles.length} vehicles.`
+   } 
+}
+
+class Menu {
+    constructor(){
+        this.department = [];
+        this.selectedDepartment = null;
+    }
+    start(){
+        let selction = this.showMainMenuOptions();
+        
+    }
+}
